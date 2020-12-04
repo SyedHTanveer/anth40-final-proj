e93ed8418d7adc03e88848d3cc4372e5b94c4b65
SyedHTanveer <Syedt21@gmail.com>
2020-12-03 21:23:18 -0500

    fixed issue where wikimedia was getting requests for all of the data rather than the one the user clicked for.

--- evomapapp/src/components/MarkerPopup.js
+++ evomapapp/src/components/MarkerPopup.js
@@ -1,27 +1,36 @@
 import React, { Component }  from 'react';
 import {Popup} from 'react-leaflet';
-import axios from 'axios';
-
 class MarkerPopup extends Component {
   constructor(props) {
     super(props);
+
+    (/\s/.test(this.props.data.tna)) ? (
+      this.state = {
+        data: this.props.data.tna.replace(/\s/g, "%20")
+      }
+  ) : (
     this.state = {
-      data: this.props.data.tna
+        data: this.props.data.tna
     }
+  )
+ 
   }
 
-  componentDidMount() {
-    axios.get('https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&titles=human&exintro=1', {
-      headers: {"Access-Control-Allow-Origin": "*"},
-      responseType: 'json',
-   }).then(response => {
-  });
+   _handleclick = () => {
+
+      const proxyurl = "https://cors-anywhere.herokuapp.com/";
+      const url = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&titles=" + this.state.data + "&exintro=1"; // site that doesn’t send Access-Control-*
+      console.log(url)
+      fetch(proxyurl + url) // https://cors-anywhere.herokuapp.com/https://example.com
+      .then(response => response.text())
+      .then(contents => console.log(contents))
+      .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
   }
 
   render() {
-    console.log(this.state.data)
-  return (
-    <Popup>
+
+      return (
+    <Popup onOpen={this._handleclick}>
       <div className='poup-text'>{this.state.data}</div>
     </Popup>
     );
