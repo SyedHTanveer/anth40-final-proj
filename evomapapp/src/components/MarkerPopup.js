112c53d8ea13491c949e279c7d045cb1fc86ea9d
SyedHTanveer <Syedt21@gmail.com>
2020-12-03 23:43:52 -0500

    fixed issue were mediawiki query wasnt working.

--- evomapapp/src/components/MarkerPopup.js
+++ evomapapp/src/components/MarkerPopup.js
@@ -1,39 +1,52 @@
 import React, { Component }  from 'react';
 import {Popup} from 'react-leaflet';
+import ReactHtmlParser from 'react-html-parser';
 class MarkerPopup extends Component {
   constructor(props) {
     super(props);
-
-    (/\s/.test(this.props.data.tna)) ? (
-      this.state = {
-        data: this.props.data.tna.replace(/\s/g, "%20")
-      }
-  ) : (
     this.state = {
-        data: this.props.data.tna
+        data: this.props.data.tna,
+        page: "",
+        search: "",
+        lat: this.props.data.lat,
+        lng: this.props.data.lng,
+        col: this.props.data.cid
     }
-  )
- 
   }
 
-   _handleclick = () => {
+  _handleclick = async () => {
+    console.log(this.props.data)
+    const proxyurl = "https://cors-anywhere.herokuapp.com/";
+    let url = "https://en.wikipedia.org/w/api.php"; 
 
-      const proxyurl = "https://cors-anywhere.herokuapp.com/";
-      const url = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&titles=" + this.state.data + "&exintro=1"; // site that doesn’t send Access-Control-*
-      console.log(url)
-      fetch(proxyurl + url) // https://cors-anywhere.herokuapp.com/https://example.com
-      .then(response => response.text())
-      .then(contents => console.log(contents))
-      .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
+    let params = {
+        action: "query",
+        list: "search",
+        srsearch: this.state.data,
+        format: "json"
+    };
+
+    url = url + "?origin=*";
+    Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
+    await fetch(proxyurl + url)
+        .then((response) => {return response.json();})
+        .then((response) => {
+           this.setState({
+             search: response.query.search[0].title,
+            page: response.query.search[0].snippet
+          });
+        })
+      .catch(function(error){console.log(error);});
   }
 
   render() {
-
-      return (
-    <Popup onOpen={this._handleclick}>
-      <div className='poup-text'>{this.state.data}</div>
-    </Popup>
-    );
+    return (
+      <Popup onOpen={this._handleclick}>
+        <div>{this.state.data + " (" + this.state.col + ")"}</div>
+        <div>{"Lat: " + this.state.lat + ", Lng: " + this.state.lng}</div>
+        <div><a href={"https://en.wikipedia.org/wiki/" + this.state.search}>{"..." + ReactHtmlParser(this.state.page) + "..."}</a></div>
+      </Popup> 
+      );
   };
 }
 
